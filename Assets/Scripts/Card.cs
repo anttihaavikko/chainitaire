2d5f0577735cde64255021eb9262149601a19f58
Antti Haavikko <antti.haavikko@focus.fi>
2020-08-20 20:48:27 +0300

    Remove event triggers from cards on placing

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Rendering;
 
 public class Card : MonoBehaviour
@@ -120,6 +121,9 @@ public class Card : MonoBehaviour
 
         if (deck.held == this)
             deck.UseHeld();
+
+        var trigger = GetComponent<EventTrigger>();
+        Destroy(trigger);
 
         PostDrop(p);
 
